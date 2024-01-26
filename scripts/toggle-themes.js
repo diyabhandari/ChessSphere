f28ce918f221ccbac16530230f999dc752bba1af
Diya Bhandari <diyabhandari@gmail.com>
2024-01-27 00:24:24 +0530

    fix piece sizing

--- scripts/toggle-themes.js
+++ scripts/toggle-themes.js
@@ -1,5 +1,29 @@
-toggleButtonElement = document.querySelector('.js-toggle-button');
-toggleMenuElement = document.querySelector('.js-toggle-menu');
+const toggleButtonElement = document.querySelector('.js-toggle-button');
+const toggleMenuElement = document.querySelector('.js-toggle-menu');
 toggleButtonElement.addEventListener('click',()=>{
   toggleMenuElement.classList.toggle('toggle-menu-on');
 })
+pageElement = document.querySelector('.page-container');
+themeOptionElement = document.querySelectorAll('.toggle-menu-option');
+if(localStorage.getItem('storedTheme')===null){
+  localStorage.setItem('storedTheme','classic');
+};
+//if theres a theme stored in ls alr from last time, it gets that instead
+const storedTheme= localStorage.getItem('storedTheme');
+pageElement.classList.add(`${storedTheme}`);
+themeOptionElement.forEach((theme)=>{
+  theme.addEventListener('click',()=>{
+    pageElement.classList.remove(`${storedTheme}`);
+    pageElement.classList.add(`${theme.innerHTML}`);
+    localStorage.setItem('storedTheme',`${theme.innerHTML}`);
+  })
+});
+
+
+
+
+
+
+
+
+
