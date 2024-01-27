69d4576cd63a22af15d5f4d9cfd92e19a2649fc0
Diya Bhandari <diyabhandari@gmail.com>
2024-01-28 01:05:29 +0530

    fix theme toggle by replacing innerHTML with dataset

--- scripts/toggle-themes.js
+++ scripts/toggle-themes.js
@@ -7,15 +7,17 @@ pageElement = document.querySelector('.page-container');
 themeOptionElement = document.querySelectorAll('.toggle-menu-option');
 if(localStorage.getItem('storedTheme')===null){
   localStorage.setItem('storedTheme','classic');
-};
+  pageElement.classList.add(`${storedTheme}`);
+}
 //if theres a theme stored in ls alr from last time, it gets that instead
-const storedTheme= localStorage.getItem('storedTheme');
-pageElement.classList.add(`${storedTheme}`);
+
 themeOptionElement.forEach((theme)=>{
   theme.addEventListener('click',()=>{
+    const storedTheme= localStorage.getItem('storedTheme');
     pageElement.classList.remove(`${storedTheme}`);
-    pageElement.classList.add(`${theme.innerHTML}`);
-    localStorage.setItem('storedTheme',`${theme.innerHTML}`);
+    pageElement.classList.add(`${theme.dataset.theme}`);
+    localStorage.setItem('storedTheme', `${theme.dataset.theme}`);
+    //console.log(`New stored theme: ${localStorage.getItem('storedTheme')}`);
   })
 });
 
